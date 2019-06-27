a31ff1183e5528705b6b638780fd9f0a2ef80798
Alan Krajina <alankrajina88@gmail.com>
2019-06-27 12:33:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -58,9 +58,6 @@ class Dog
   
   def self.new_from_db(row)
   	pat = Dog.new(id:row[0],name:row[1],breed:row[2])
-  	pat.id = :id
-  	pat.name =  :name
-  	pat.breed = :breed
   	pat    
   end
   
